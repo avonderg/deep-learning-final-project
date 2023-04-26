b99d2d97d96157acae4a94aaa4ca1eb49b5571f3
avonderg <77714730+avonderg@users.noreply.github.com>
2023-04-26 10:32:44 -0400

    started lstm

--- code/assignment.py
+++ code/assignment.py
@@ -1,13 +1,18 @@
 from __future__ import absolute_import
 from matplotlib import pyplot as plt
 from preprocess import get_data
-from convolution import conv2d
+from lstm import LSTM
+# from convolution import conv2d
 
 import os
 import tensorflow as tf
 import numpy as np
 import random
 import math
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 # ensures that we run only on cpu
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
@@ -15,6 +20,38 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 window_size = 60
 test_samples = 251
 future_time_steps = 20
+
+
+def train_lstm(model, input_data, target_data, learning_rate=0.001, num_epochs=100):
+    # Define the loss function and the optimizer
+    loss_function = nn.MSELoss()  # Mean squared error loss
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Training loop
+    for epoch in range(num_epochs):
+        # Clear the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        output = model(input_data)
+
+        # Reshape output and target tensors for loss calculation
+        output = output.view(-1, model.hidden2tag.out_features)  # (batch_size * sequence_length, tagset_size)
+        target = target_data.view(-1)  # (batch_size * sequence_length)
+
+        # Calculate the loss
+        loss = loss_function(output, target)
+
+        # Backward pass
+        loss.backward()
+
+        # Update the weights
+        optimizer.step()
+
+        # Print the loss for this epoch
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+
 
 # class Model(tf.keras.Model):
 #     def __init__(self):
@@ -284,8 +321,36 @@ def main():
     
     :return: None
     '''
+
  
     trainX, trainY, testX, testY, X, min_max = get_data(window_size, test_samples, future_time_steps)
+
+
+    # Assuming the get_data function returns trainX, trainY, testX, testY, X, and min_max
+    window_size = 50
+    test_samples = 100
+    future_time_steps = 10
+
+    trainX, trainY, testX, testY, X, min_max = get_data(window_size, test_samples, future_time_steps)
+
+    # Convert the numpy arrays to PyTorch tensors and reshape them accordingly
+    trainX_tensor = torch.tensor(trainX).float().unsqueeze(-1)  # (batch_size, sequence_length, input_dim)
+    trainY_tensor = torch.tensor(trainY).float()  # (batch_size, output_dim)
+    testX_tensor = torch.tensor(testX).float().unsqueeze(-1)  # (batch_size, sequence_length, input_dim)
+    testY_tensor = torch.tensor(testY).float()  # (batch_size, output_dim)
+
+    # Create an instance of the LSTMTagger model
+    embedding_dim = 100
+    hidden_dim = 128
+    vocab_size = 5000
+    tagset_size = 10
+
+    model = LSTM(embedding_dim, hidden_dim, vocab_size, tagset_size)
+
+    # Train the model
+    train_lstm(model, trainX_tensor, trainY_tensor, learning_rate=0.001, num_epochs=200)
+
+
     # model = Model()
 
     # for epoch in range(model.epochs):
